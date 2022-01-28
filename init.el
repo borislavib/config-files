b537323ff09197e7669ee2432db2773caef2098c
bb1 <borib97@gmail.com>
2022-01-28 09:08:33 +0200

    clean up

--- init.el
+++ init.el
@@ -1,60 +1,11 @@
-(package-initialize)
- 
-(custom-set-faces
- '(default ((t (:family "Ubuntu Mono" :foundry "DAMA" :slant normal :weight normal :height 158 :width normal)))))
+;; no extra packages i.e. powerline 
+;; 
 
-
-(when (>= emacs-major-version 24)
-  (require 'package)
-  (add-to-list
-   'package-archives
-   '("melpa" . "http://melpa.org/packages/")
-   t)
-  (package-initialize))
-
-(blink-cursor-mode 0)
-
-;; Don't use messages that you don't read
-(setq initial-scratch-message "")
-(setq inhibit-startup-message t)
-
-(setq visible-bell t)
+;; (setq org-agenda-files '("/")) 
+;; (setq org-log-done 'time)
 
 ;; This is bound to f11 in Emacs 24.4
 ;(toggle-frame-fullscreen) 
-
-;; use F10 instead
-(menu-bar-mode 0)
-
-;; Who uses the bar to scroll?
-(scroll-bar-mode 0)
-
-; default file C-x f
-(setq default-directory "/" ) 
-
-(add-hook 'text-mode-hook 'turn-on-auto-fill)
-(add-hook 'text-mode-hook
-  '(lambda() (set-fill-column 100)))
-
-(setq cua-enable-cua-keys nil)
-(cua-mode)
-
-(tool-bar-mode -1)
-
-(show-paren-mode)
-
-(column-number-mode)
-
-(scroll-bar-mode -1)
-
-;; C-c <arrow> for changing buffer  
-(winner-mode t)
-
-(global-hl-line-mode)
-
-(display-time-mode 1)
-
-(global-set-key (kbd "M-[") 'ace-window)
 
 ;; https://stackoverflow.com/questions/2580650/how-can-i-reload-emacs-after-changing-it
 ;; M-x eval buffer
@@ -66,14 +17,6 @@
 ;; go to line
 ;; M-g g (goto-line)
 
-;; https://www.emacswiki.org/emacs/WindowResize 
-;; Resize window 
-(global-set-key (kbd "S-C-<left>") 'shrink-window-horizontally)
-(global-set-key (kbd "S-C-<right>") 'enlarge-window-horizontally)
-(global-set-key (kbd "S-C-<down>") 'shrink-window)
-(global-set-key (kbd "S-C-<up>") 'enlarge-window)
-
-
 ;; Kill current buffer 
 ;; C-x k
 ;; https://stackoverflow.com/questions/207510/what-is-the-key-sequence-for-closing-the-current-buffer-in-emacs
@@ -82,39 +25,6 @@
 ;; C-h v package-activated-list
 ;; https://stackoverflow.com/questions/13866848/how-to-save-a-list-of-all-the-installed-packages-in-emacs-24
 
-
-;; adjust paragraph length  
-;; M-q
-(setq-default fill-column 100)
-
-
-;; https://orgmode.org/worg/org-tutorials/encrypting-files.html
-(require 'epa-file)
-(epa-file-enable)
-
-
-;; https://stackoverflow.com/questions/69934/set-4-space-indent-in-emacs-in-text-mode
-(setq-default indent-tabs-mode nil)
-(setq-default tab-width 4)
-(setq indent-line-function 'insert-tab)
-
-;; https://www.reddit.com/r/emacs/comments/3s7d38/im_constantly_saving_my_changes_with_cx_cs_do_you/
-(global-set-key [f4] 'save-buffer)
-
-;; https://stackoverflow.com/questions/12558019/shortcut-to-open-a-specific-file-in-emacs
-(global-set-key (kbd "<f5>") (lambda() (interactive)(find-file "~/Documents/")))
-
-;; M-S <up/down> for moving paragraphs up and down 
-
-;; stop creating backup~ files
-;; http://ergoemacs.org/emacs/emacs_set_backup_into_a_directory.html
-(setq make-backup-files nil) 
-;; stop creating #autosave# files
-(setq auto-save-default nil) 
-
-;; start calendar week from monday 
-;; https://emacs.stackexchange.com/questions/42571/org-agenda-date-prompt-mini-calendar-start-week-on-monday
-(setq calendar-week-start-day 1)
 
 ;; init file 
 ;; On Microsoft Windows:
@@ -140,6 +50,118 @@
 
 ;; s-arrow 
 ;; change list from 1) -> - -> 1. -> + 
+
+;; M-S <up/down> for moving paragraphs up and down 
+
+
+;; https://emacs.stackexchange.com/questions/9465/how-to-shift-a-selected-area-of-text-in-emacs-by-a-certain-number-of-spaces
+;; C-5 C-x TAB
+
+;; shortcuts 
+;; C-/ undo 
+
+
+
+
+
+
+
+(package-initialize)
+ 
+(custom-set-faces
+ '(default ((t (:family "Ubuntu Mono" :foundry "DAMA" :slant normal :weight normal :height 158 :width normal)))))
+
+
+(when (>= emacs-major-version 24)
+  (require 'package)
+  (add-to-list
+   'package-archives
+   '("melpa" . "http://melpa.org/packages/")
+   t)
+  (package-initialize))
+
+(blink-cursor-mode 0)
+
+;; Don't use messages that you don't read
+(setq initial-scratch-message "")
+(setq inhibit-startup-message t)
+
+(setq visible-bell t)
+
+
+;; use F10 instead
+(menu-bar-mode 0)
+
+;; Who uses the bar to scroll?
+(scroll-bar-mode 0)
+
+; default file C-x f
+(setq default-directory "/" ) 
+
+(add-hook 'text-mode-hook 'turn-on-auto-fill)
+(add-hook 'text-mode-hook
+  '(lambda() (set-fill-column 100)))
+
+;; column selection 
+(setq cua-enable-cua-keys nil)
+(cua-mode)
+
+(tool-bar-mode -1)
+
+(show-paren-mode)
+
+(column-number-mode)
+
+(scroll-bar-mode -1)
+
+;; C-c <arrow> for changing buffer  
+(winner-mode t)
+
+(global-hl-line-mode)
+
+(display-time-mode 1)
+
+(global-set-key (kbd "M-[") 'ace-window)
+
+
+;; https://www.emacswiki.org/emacs/WindowResize 
+;; Resize window 
+(global-set-key (kbd "S-C-<left>") 'shrink-window-horizontally)
+(global-set-key (kbd "S-C-<right>") 'enlarge-window-horizontally)
+(global-set-key (kbd "S-C-<down>") 'shrink-window)
+(global-set-key (kbd "S-C-<up>") 'enlarge-window)
+
+;; adjust paragraph length  
+;; M-q
+(setq-default fill-column 100)
+
+
+;; https://orgmode.org/worg/org-tutorials/encrypting-files.html
+(require 'epa-file)
+(epa-file-enable)
+
+
+;; https://stackoverflow.com/questions/69934/set-4-space-indent-in-emacs-in-text-mode
+(setq-default indent-tabs-mode nil)
+(setq-default tab-width 4)
+(setq indent-line-function 'insert-tab)
+
+;; https://www.reddit.com/r/emacs/comments/3s7d38/im_constantly_saving_my_changes_with_cx_cs_do_you/
+(global-set-key [f4] 'save-buffer)
+
+;; https://stackoverflow.com/questions/12558019/shortcut-to-open-a-specific-file-in-emacs
+(global-set-key (kbd "<f5>") (lambda() (interactive)(find-file "~/Documents/")))
+
+;; stop creating backup~ files
+;; http://ergoemacs.org/emacs/emacs_set_backup_into_a_directory.html
+(setq make-backup-files nil) 
+
+;; stop creating #autosave# files
+(setq auto-save-default nil) 
+
+;; start calendar week from monday 
+;; https://emacs.stackexchange.com/questions/42571/org-agenda-date-prompt-mini-calendar-start-week-on-monday
+(setq calendar-week-start-day 1)
 
 ;; Use system clipboard
 (setq x-select-enable-clipboard t)
@@ -173,13 +195,6 @@
 (fset 'yes-or-no-p 'y-or-n-p)
 
 
-;; https://emacs.stackexchange.com/questions/9465/how-to-shift-a-selected-area-of-text-in-emacs-by-a-certain-number-of-spaces
-;; C-5 C-x TAB
-
-;; shortcuts 
-;; C-/ undo 
-
-
 ;; http://pragmaticemacs.com/emacs/scrolling-and-moving-by-line/
 ;;keep cursor at same position when scrolling
 (setq scroll-preserve-screen-position 1)
@@ -188,3 +203,16 @@
 (global-set-key (kbd "M-p") (kbd "C-u 1 M-v"))
 
 
+
+
+;; This is for org-mode agenda view activation
+ (add-to-list 'auto-mode-alist '("\\.org\\'" . org-mode))
+;; This is for key bindings to invoke agenda mode (see line-2)
+;; (global-set-key "\C-cl" 'org-store-link)
+ (global-set-key [f3] 'org-agenda)
+;; (global-set-key "\C-cc" 'org-capture)
+;; (global-set-key "\C-cb" 'org-iswitchb)
+
+
+; agenda file where to search
+; (setq org-agenda-files '("/")) 
